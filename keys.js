03f5fe179d61722c5b14c15f7ed82e1f2d0d0564
Kyle Robert <notoriouskro@gmail.com>
2019-03-02 23:08:02 -0800

    functions work

--- keys.js
+++ keys.js
@@ -5,6 +5,3 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
-var keys = require("./keys.js");
-
-var spotify = new Spotify(keys.spotify);
--- liri.js
+++ liri.js
@@ -1,21 +1,50 @@
 require("dotenv").config();
-
+var fs = require("fs")
+var keys = require("./keys.js");
+var spotify = require('node-spotify-api');
 var axios = require("axios");
+// var moment = require('moment');
 var cmd = process.argv[2];
 var cmdData = process.argv[3];
-cmdData = cmdData.splice(' ').join('+');
+cmdData = cmdData.split(' ').join('+');
+// var textFile = "log.txt";
 
-if(cmd === 'concert-this'){
-    console.log('concert-this');
+if (cmd === 'concert-this') {
+  console.log(cmd);
+  var artist = process.argv[3];
+  artist = artist.split(' ').join('')
 
-}else if(cmd === 'spotify-this-song'){
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&tracker_count=5";
+ 
 
-    console.log('spotify-this-song');
+  axios.get(queryUrl).then(function (response) {
     
+     console.log(response.data)
+    // console.log("----------------------");
+    // console.log("Venue: " + event.venue.name);
+    // console.log("Location: " + event.venue.city);
+    // console.log("Date: " + response.tracks.items[0].preview_url);
+  });
 
-}else if(cmd === 'movie-this'){
+} else if (cmd === 'spotify-this-song') {
 
-    console.log('movie-this');
+  var spotify = new spotify(keys.spotify);
+  spotify
+    .search({ type: 'track', query: cmdData })
+    .then(function (response) {
+      console.log("----------------------");
+      console.log("Artist: " + response.tracks.items[0].artists[0].name);
+      console.log("Track Name: " + response.tracks.items[0].name);
+      console.log("URL: " + response.tracks.items[0].preview_url);
+      console.log("Album: " + response.tracks.items[0].album.name);
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+
+} else if (cmd === 'movie-this') {
+ 
+
   //  * Title of the movie.
   //  * Year the movie came out.
   //  * IMDB Rating of the movie.
@@ -25,47 +54,50 @@ if(cmd === 'concert-this'){
   //  * Plot of the movie.
   //  * Actors in the movie.
 
+ 
+  var movie = process.argv.slice(3).join('+');
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
   axios
-  .get("http://www.omdbapi.com/?t=" + cmdData + "&y=&plot=short&apikey=trilogy")
-  .then(function(response) {
-    // If the axios was successful...
-    // Then log the body from the site!
-    console.log(response.data);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code$ npm install axios
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-    
-  });
+    .get(queryUrl).then(function (response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log("----------------------");
+      console.log("Movie title: " + response.data.Title);
+      console.log("Movie released in: " + response.data.Year);
+      console.log("IMDB: " + response.data.imdbRating);
+      console.log("Rotten Tomatoes: " + response.data.rtRating);
+      console.log("Country: " + response.data.Country);
+      console.log("Language(s): " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code$ npm install axios
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+
+    });
 
 
-}else if(cmd === 'do-what-it-says'){
-    console.log('do-what-it-says');
+} else if (cmd === 'do-what-it-says') {
+  console.log('do-what-it-says');
 };
 
-var Spotify = require('node-spotify-api');
- 
-spotify
-  .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+
 
 
 
